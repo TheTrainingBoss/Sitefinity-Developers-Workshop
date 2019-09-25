cec07e51302bde9feb16cbb16eeb9d3b47b787a9
Lino Tadros <lino@eventsxd.com>
2019-09-25 16:28:46 -0400

    Update

--- Source/SFDevWorkshop/packages/RazorGenerator.Templating.2.1.1/Tools/install.ps1
+++
@@ -1,5 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-
-# set the test page's custom tool to the razor generator
-$testPage = $project.ProjectItems.Item("SampleTemplate.cshtml")
-$testPage.Properties.Item("CustomTool").Value = "RazorGenerator"
